d9d487561cabf166da752ad67fff4d9b49fdcc49
Верховод Никита Сергеевич <nsverhovod@mts.ru>
2024-10-22 18:48:47 +0300

    adding Scanner and Makefile in project

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,9 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
+
+
         System.out.println("Welcome to the Brain Games!");
+        Cli.startApp();
     }
 }
