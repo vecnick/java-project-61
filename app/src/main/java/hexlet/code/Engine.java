0594c2e8c81605f3e0d0948ca40ac5c233332b66
Верховод Никита Сергеевич <nsverhovod@mts.ru>
2024-11-06 19:38:15 +0300

    fix project after review

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -9,7 +9,6 @@ public class Engine {
     public static void game(String rule, GameRound[] rounds) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
